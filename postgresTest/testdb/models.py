dbef1a0f3bee918ec0f2af11527c0552fccc73fd
LAPTOP-CN4HVAQM\W10H-X64 <ktrakunlertrat@gmail.com>
2024-05-27 12:10:58 +0700

    update 27/5/67 12.11

--- postgresTest/testdb/models.py
+++ postgresTest/testdb/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Teacher(models.Model):
     name = models.CharField(max_length=80)
     age = models.IntegerField()
+
+class Post(models.Model):
+    name = models.CharField(max_length=200)
+    desc=models.TextField()
